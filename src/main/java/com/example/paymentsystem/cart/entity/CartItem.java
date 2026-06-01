d43b23f52e5959f57988b0c01d75af90455b7e4b
eashhyeon3232 <jihyeon-lou3232@naver.com>
2026-06-01 19:08:08 +0900

    feat : 장바구니 상품 엔티티 구현
    - CartItem 엔티티 샏ㅇ성 및 Product, Cart 연관관계(LAZY) 매핑
    - 장바구니 내 동일 상품 중복 방지를 위한 복합 유니크 제약조건 설정
    - 수량 변경 및 변경 시 1개 이상 검증 기능 구현((BusinessException 적용)
    - JPA 데이터 정수(int UNSIGNED) 기본값 설정

---
+++ src/main/java/com/example/paymentsystem/cart/entity/CartItem.java
@@ -0,0 +1,67 @@
+package com.example.paymentsystem.cart.entity;
+
+import com.example.paymentsystem.global.common.BaseEntity;
+import com.example.paymentsystem.global.error.BusinessException;
+import com.example.paymentsystem.global.error.ErrorCode;
+import com.example.paymentsystem.product.Product;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static com.example.paymentsystem.global.error.ErrorCode.INVALID_QUANTITY;
+
+@Entity
+@Table(name = "cart_items", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"cart_id", "product_id"})
+})
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CartItem extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @Column(nullable = false, columnDefinition = "int UNSIGNED DEFAULT 1")
+    private int quantity;
+
+    public CartItem(Product product, Cart cart, int quantity) {
+        this.cart = cart;
+        this.product = product;
+        if(quantity < 1) {
+            throw new IllegalArgumentException("수량은 1이상여야 합니다.");
+        }
+        this.quantity = quantity;
+    }
+
+    public Long getCartId() {
+        return cart.getId();
+    }
+
+    public Long getProductId() {
+        return product.getId();
+    }
+
+    public void addQuantity(int quantity) {
+        if(quantity < 1) {
+            throw new BusinessException(INVALID_QUANTITY);
+        }
+        this.quantity += quantity;
+    }
+
+    public void changeQuantity(int quantity) {
+        if(quantity < 1) {
+            throw new BusinessException(INVALID_QUANTITY);
+        }
+        this.quantity = quantity;
+    }
+}

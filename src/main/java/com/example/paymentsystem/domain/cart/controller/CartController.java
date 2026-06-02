85a6ef11a11a24a960c466885078dca9eadb041e
easyhyeon3232 <146900921+easyhyeon3232@users.noreply.github.com>
2026-06-02 20:51:16 +0900

    feat: 회원별 장바구니 목록 조회 API 구현 (#7)
    * feat : 장바구니 상품 엔티티 구현
    
    - CartItem 엔티티 샏ㅇ성 및 Product, Cart 연관관계(LAZY) 매핑
    - 장바구니 내 동일 상품 중복 방지를 위한 복합 유니크 제약조건 설정
    - 수량 변경 및 변경 시 1개 이상 검증 기능 구현((BusinessException 적용)
    - JPA 데이터 정수(int UNSIGNED) 기본값 설정
    
    * refactor : 사용되지 않는 import문 제거
    
    * feat : CartItem 엔티티 내 정적 팩토리 메서드(create) 및 수량 검증 로직 추가
    
    - 장바구니 상품 생성을 전담하는 create() 메서드 구현
    - 상품 담기 시 수량이 0 이하일 경우 feat : 장바구니 상품 엔티티 구현
    
    - CartItem 엔티티 샏ㅇ성 및 Product, Cart 연관관계(LAZY) 매핑
    - 장바구니 내 동일 상품 중복 방지를 위한 복합 유니크 제약조건 설정
    - 수량 변경 및 변경 시 1개 이상 검증 기능 구현(BusinessException 적용)
    - JPA 데이터 정수(int UNSIGNED) 기본값 설정(INVALID_QUANTITY) 예외 처리 추가
    
    * feat : 장바구니(Cart) 엔티티 추가 및 회원 연관관계 매핑
    
    - Member 엔티티와 일대일 단방향 연관관계 매핑 적용
    - 데이터 member_id 유니크 제약 조건 설정
    - Cart.create 정적 팩토리 메서드 구현
    
    * feat : CartItemRepository 내 장바구니 조회 및 삭제 쿼리 구현
    
    * feat : CartRepository 구현 및 회원 ID기반 장바구니 조회 메서드 추가
    
    * feat : CartItemRepository 내 장바구니 조회 및 삭제 쿼리 구현
    
    * feat : CartRepository 구현 및 회원 ID기반 장바구니 조회 메서드 추가
    
    * feat : CartController 구현 및 장바구니 상품  추가 API 엔트포인트 생성
    
    * feat : CartItem 엔티티 내 정적 팩토리 메서드 구현 및 예외 처리 통일
    
    * feat : Cart 엔티티 정적 팩토리 메서드 추가
    
    * feat : CartService 내 재고 검증 로직 추가 및 장바구니 상품 담기 구현
    
    * feat : 장바구니 상품 담기 DTO 구현
    
    * refactor : CartController 내 공통 응답(ApiResponse) 및 201 Created 상태 코드 적용
    
    * refactor : CartService 내 장바구니 상품 추가 로직 가독성 개선
    
    * refactor : CartService 내 장바구니 상품 추가 로직 가독성 개선
    
    * feat: 장바구니 조회 API 응답 규격 적용 (ResponseEntity, ApiResponse)
    
    * feat: 장바구니 Response DTO(CartResponse, CartItemResponse) 구현
    
    * feat: 회원별 장바구니 목록 조회 서비스 로직 구현
    
    * feat: ErrorCode 이넘에 CART_NOT_FOUND 상수 추가
    
    * feat: Order 엔티티 기본 셋팅
    
    * feat: OrderRepository 기본 셋팅
    
    * feat: OrderStatus 기본 셋팅
    
    * refactor : 사용되지 않는 import문 제거

--- src/main/java/com/example/paymentsystem/domain/cart/controller/CartController.java
+++ src/main/java/com/example/paymentsystem/domain/cart/controller/CartController.java
@@ -2,16 +2,14 @@ package com.example.paymentsystem.domain.cart.controller;
 
 import com.example.paymentsystem.domain.cart.dto.AddCartRequest;
 import com.example.paymentsystem.domain.cart.dto.AddCartResponse;
+import com.example.paymentsystem.domain.cart.dto.CartResponse;
 import com.example.paymentsystem.domain.cart.service.CartService;
 import com.example.paymentsystem.global.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/carts")
@@ -23,6 +21,12 @@ public class CartController {
     // todo JWT 구현 완료 후 memberId는 인증 객체에서 꺼내도록 수정 예정
     private static final Long MEMBER_ID = 1L;
 
+    @GetMapping
+    public ResponseEntity<ApiResponse<CartResponse>> findCartItems() {
+        CartResponse response = cartService.findCartItems(MEMBER_ID);
+
+        return ResponseEntity.ok(ApiResponse.ok(response));
+    }
 
     @PostMapping("/items")
     public ResponseEntity<ApiResponse<AddCartResponse>> addItem(@Valid @RequestBody AddCartRequest request) {

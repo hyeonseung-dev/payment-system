38d475c461c134a78b07849ffea5c71f4849129b
t2025-m0056 <dlkfjdsklf1010@gmail.com>
2026-06-02 20:44:07 +0900

    ProductController 수정

--- src/main/java/com/example/paymentsystem/domain/product/controller/ProductController.java
+++ src/main/java/com/example/paymentsystem/domain/product/controller/ProductController.java
@@ -3,7 +3,9 @@ package com.example.paymentsystem.domain.product.controller;
 import com.example.paymentsystem.domain.product.dto.response.ProductDetailResponse;
 import com.example.paymentsystem.domain.product.dto.response.ProductPageResponse;
 import com.example.paymentsystem.domain.product.service.ProductService;
+import com.example.paymentsystem.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,11 +20,20 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
-    public ResponseEntity<ProductPageResponse> getProducts()
-    {return ResponseEntity.ok(productService.getProducts());}
+    public ResponseEntity<ApiResponse<ProductPageResponse>> getProducts() {
+        ProductPageResponse response = productService.getProducts();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.ok(response));
+    }
 
     @GetMapping("/{productId}")
-    public ResponseEntity<ProductDetailResponse> getProduct(@PathVariable Long productId)
-    {return ResponseEntity.ok(productService.getProduct(productId));}
+    public ResponseEntity<ApiResponse<ProductDetailResponse>> getProduct(@PathVariable Long productId) {
+        ProductDetailResponse response = productService.getProduct(productId);
 
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.ok(response));
+    }
 }

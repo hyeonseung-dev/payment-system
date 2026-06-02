edd341b95d849c99f3ed2e8f769c03cb00a63921
easyhyeon3232 <146900921+easyhyeon3232@users.noreply.github.com>
2026-06-02 16:04:56 +0900

    feat: 장바구니 상품 추가 API 구현 (공통 응답 및 명세 변경 확인 요청) (#6)
    * feat : 장바구니 상품 엔티티 구현
    
    - CartItem 엔티티 샏ㅇ성 및 Product, Cart 연관관계(LAZY) 매핑
    - 장바구니 내 동일 상품 중복 방지를 위한 복합 유니크 제약조건 설정
    - 수량 변경 및 변경 시 1개 이상 검증 기능 구현((BusinessException 적용)
    - JPA 데이터 정수(int UNSIGNED) 기본값 설정
    
    * refactor : 사용되지 않는 import문 제거
    
    * feat : CartItem 엔티티 내 정적 팩토리 메서드(create) 및 수량 검증 로직 추가
    
    - 장바구니 상품 생성을 전담하는 create() 메서드 구현
    - 상품 담기 시 수량이 0 이하일 경우 feat : 장바구니 상품 엔티티 구현
    
    - CartItem 엔티티 샏ㅇ성 및 Product, Cart 연관관계(LAZY) 매핑
    - 장바구니 내 동일 상품 중복 방지를 위한 복합 유니크 제약조건 설정
    - 수량 변경 및 변경 시 1개 이상 검증 기능 구현(BusinessException 적용)
    - JPA 데이터 정수(int UNSIGNED) 기본값 설정(INVALID_QUANTITY) 예외 처리 추가
    
    * feat : 장바구니(Cart) 엔티티 추가 및 회원 연관관계 매핑
    
    - Member 엔티티와 일대일 단방향 연관관계 매핑 적용
    - 데이터 member_id 유니크 제약 조건 설정
    - Cart.create 정적 팩토리 메서드 구현
    
    * feat : CartItemRepository 내 장바구니 조회 및 삭제 쿼리 구현
    
    * feat : CartRepository 구현 및 회원 ID기반 장바구니 조회 메서드 추가
    
    * feat : CartItemRepository 내 장바구니 조회 및 삭제 쿼리 구현
    
    * feat : CartRepository 구현 및 회원 ID기반 장바구니 조회 메서드 추가
    
    * feat : CartController 구현 및 장바구니 상품  추가 API 엔트포인트 생성
    
    * feat : CartItem 엔티티 내 정적 팩토리 메서드 구현 및 예외 처리 통일
    
    * feat : Cart 엔티티 정적 팩토리 메서드 추가
    
    * feat : CartService 내 재고 검증 로직 추가 및 장바구니 상품 담기 구현
    
    * feat : 장바구니 상품 담기 DTO 구현
    
    * refactor : CartController 내 공통 응답(ApiResponse) 및 201 Created 상태 코드 적용

---
+++ src/main/java/com/example/paymentsystem/domain/cart/repository/CartItemRepository.java
@@ -0,0 +1,22 @@
+package com.example.paymentsystem.domain.cart.repository;
+
+import com.example.paymentsystem.domain.cart.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    @Query("SELECT ci FROM CartItem ci join fetch ci.product where ci.cart.member.id = :memberId")
+    List<CartItem> findAllByMemberId(@Param("memberId") Long memberId);
+
+    Optional<CartItem> findByCart_Member_IdAndProduct_Id(Long memberId, Long productId);
+
+    @Modifying
+    @Query("DELETE FROM CartItem ci WHERE ci.id = :id AND ci.cart.member.id = :memberId")
+    int deleteByIdAndCart_Member_Id(@Param("id") Long id, @Param("memberId") Long memberId);
+
+}

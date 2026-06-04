9117ae4c3abdb60968f26006e9845c5332dd4a2b
HS <ehehqp@gmail.com>
2026-06-04 12:31:52 +0900

    fix: 엔티티 어노테이션 등 기본 설정 추가

--- src/main/java/com/example/paymentsystem/domain/order/entity/OrderItem.java
+++ src/main/java/com/example/paymentsystem/domain/order/entity/OrderItem.java
@@ -2,8 +2,19 @@ package com.example.paymentsystem.domain.order.entity;
 
 import com.example.paymentsystem.global.common.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+/**
+ * 주문 상품의 상품 스냅샷과 주문 수량을 관리하는 엔티티이다.
+ */
+@Getter
+@Entity
+@Table(name = "order_items")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;

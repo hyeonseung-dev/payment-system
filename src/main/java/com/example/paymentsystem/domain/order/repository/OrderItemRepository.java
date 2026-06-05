adea9e7d00ce36f852a42930b60cec82d1861ecd
eashhyeon3232 <jihyeon-lou3232@naver.com>
2026-06-05 20:50:46 +0900

    feat : 주문 목록 상품 조회 쿼리 추가

--- src/main/java/com/example/paymentsystem/domain/order/repository/OrderItemRepository.java
+++ src/main/java/com/example/paymentsystem/domain/order/repository/OrderItemRepository.java
@@ -2,10 +2,13 @@ package com.example.paymentsystem.domain.order.repository;
 
 import com.example.paymentsystem.domain.order.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
-    List<OrderItem> findAllByOrder_Id(Long orderId);
+    @Query("SELECT oi FROM OrderItem oi WHERE oi.order.id IN :orderIds ORDER BY oi.id ASC")
+    List<OrderItem> findAllByOrderIds(@Param("orderIds")List<Long> orderIds);
 }

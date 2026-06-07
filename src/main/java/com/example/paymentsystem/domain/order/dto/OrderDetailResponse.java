310e2f973e239876cb726ac2508b9a3d1f510486
eashhyeon3232 <jihyeon-lou3232@naver.com>
2026-06-07 21:47:29 +0900

    feat : 주문 상세 응답 DTO 추가

---
+++ src/main/java/com/example/paymentsystem/domain/order/dto/OrderDetailResponse.java
@@ -0,0 +1,49 @@
+package com.example.paymentsystem.domain.order.dto;
+
+import com.example.paymentsystem.domain.order.entity.Order;
+import com.example.paymentsystem.domain.order.entity.OrderItem;
+import com.example.paymentsystem.domain.order.enums.OrderStatus;
+import com.example.paymentsystem.domain.payment.entity.Payment;
+import com.example.paymentsystem.domain.payment.entity.PaymentStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record OrderDetailResponse (
+        Long orderId,
+        String orderNumber,
+        OrderStatus status,
+        int totalAmount,
+        int pointAmount,
+        int pgAmount,
+        LocalDateTime createAt,
+        PaymentStatus paymentStatus,
+        Long earnedPointAmount,
+        List<OrderDetailItemResponse> items
+) {
+    public static OrderDetailResponse of(Order order, List<OrderItem> orderItems, Payment payment) {
+        // 주문 생성 시점에 저장한 포인트 사용 금액
+        int pointAmount = order.getUsePointAmountSnapshot();
+
+        // 전체 주문 금액에서 포인트 사용 금액을 뺀 실제 PG 결제 금액
+        int pgAmount = order.getTotalAmount() - pointAmount;
+
+        // 주문 상품 엔티티를 상세 응답 DTO로 변환
+        List<OrderDetailItemResponse> items = orderItems.stream()
+                .map(OrderDetailItemResponse::from)
+                .toList();
+
+        return new OrderDetailResponse(
+                order.getId(),
+                order.getOrderNumber(),
+                order.getStatus(),
+                order.getTotalAmount(),
+                pointAmount,
+                pgAmount,
+                order.getCreatedAt(),
+                payment.getStatus(),
+                payment.getEarnedPointAmount(),
+                items
+        );
+    }
+}

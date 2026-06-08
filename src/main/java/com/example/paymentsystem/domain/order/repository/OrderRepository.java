89101521f6e8e1a4dc0320858d1bf1772155eea1
easyhyeon3232 <146900921+easyhyeon3232@users.noreply.github.com>
2026-06-08 10:39:29 +0900

    feat: 주문 상세 조회 API 구현 (주문 상품 및 결제 정보 연동) (#29)
    * feat : 주문 목록 아이템 응답 DTO 추가
    
    * feat : 주문 목록 페이지 응답 DTO 추가
    
    * feat : 회원별 주문 목록 조회 쿼리 추가
    
    * feat : 주문 목록 상품 조회 쿼리 추가
    
    * feat : 주문 목록 조회 로직 추가
    
    * feat : 주문 목록 조회 API 추가
    
    * test : 주문 목록 조회 테스트 추가
    
    * feat : 주문 목록 아이템 응답 DTO 추가
    
    * feat : 주문 목록 페이지 응답 DTO 추가
    
    * feat : 회원별 주문 목록 조회 쿼리 추가
    
    * feat : 주문 목록 상품 조회 쿼리 추가
    
    * feat : 주문 목록 조회 로직 추가
    
    * feat : 주문 목록 조회 API 추가
    
    * test : 주문 목록 조회 테스트 추가
    
    * fix : 수정 안된 부분 다시 수정하기
    
    * feat : 주문 상세 응답 DTO 추가
    
    * feat : 회원별 주문 상세 조회, 주문 상세 상품 목록 죄회, 주문 ID 기반 결제 조회 쿼리 추가
    
    * feat : 주문 상세 조회 로직 추가
    
    * feat : 주문 상세 조회 API 추가
    
    * test : 주문 상세 조회 테스트 추가
    
    * Refactor/order get (#30)
    
    * refactor : getTotalAmount() - pointAmount로 추측계산말고 결제 도메인에 실제 저장된 payment.getPgAmount()를 사용으로 수정
    
    * refactor : 목록 조회에 사용하지 않는 pgAmount 삭제 수정
    
    * Refactor/order get (#31)
    
    * refactor : getTotalAmount() - pointAmount로 추측계산말고 결제 도메인에 실제 저장된 payment.getPgAmount()를 사용으로 수정
    
    * refactor : 목록 조회에 사용하지 않는 pgAmount 삭제 수정
    
    * test : 주문 조회 응답 변경에 맞게 테스트 수정
    
    * Update OrderDetailResponse.java

--- src/main/java/com/example/paymentsystem/domain/order/repository/OrderRepository.java
+++ src/main/java/com/example/paymentsystem/domain/order/repository/OrderRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     // 로그인한 회원의 주문 목록을 최신순으로 조회
     Page<Order> findByMember_IdOrderByCreatedAtDesc(Long memberId, Pageable pageable);
+
+    Optional<Order> findByIdAndMember_Id(Long orderId, Long memberId);
 }
